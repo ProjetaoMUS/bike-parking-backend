3db9a7a76ab190bfd3e866cdfcbfe6be778d7b79
hlaff147 <hlaff@cin.ufpe.br>
2023-09-06 15:22:54 -0300

    Ajusta serializers.py User

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'tax_id', 'phone_number', 'email', 'first_name', 'gender', 'is_active', 'is_staff', 'is_superuser']
+        fields = ['id', 'tax_id', 'phone_number', 'email', 'first_name', 'gender', 'is_active', 'is_staff', 'is_superuser', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
