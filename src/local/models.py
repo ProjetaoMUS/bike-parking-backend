efa5b7bcdd00e7724efcee0089f2bf093b63fd5a
hlaff147 <hlaff@cin.ufpe.br>
2023-09-09 20:21:46 -0300

    Refatora modelo Local e atualiza nomes dos campos para inglês, alinhando com padrão do código.

--- src/local/models.py
+++ src/local/models.py
@@ -5,14 +5,14 @@ from django.db import models
 
 
 class Local(models.Model):
-    nome = models.CharField(max_length=255)
-    numero_de_vagas = models.PositiveIntegerField()
-    imagens = models.ImageField(upload_to='local/imagens/', blank=True, null=True)
-    localizacao = models.CharField(max_length=255)
-    descricao = models.TextField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    lat = models.DecimalField(max_digits=9, decimal_places=6)
-    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    name = models.CharField(max_length=255)
+    parking_spaces_count = models.PositiveIntegerField()
+    images = models.ImageField(upload_to='local/imagens/', blank=True, null=True)
+    address = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
     def __str__(self):
-        return self.nome
+        return self.name
