8ca3bd4b1b680be3ad541a54fbc608b9d81f6898
hlaff147 <hlaff@cin.ufpe.br>
2023-09-27 19:56:32 -0300

    Added rating field to PartnerLocation model

--- src/partner_location/models.py
+++ src/partner_location/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
@@ -9,6 +10,7 @@ class PartnerLocation(models.Model):
     parking_spaces_count = models.PositiveIntegerField()
     images = models.ImageField(upload_to='partner_location/imagens/', blank=True, null=True)
     address = models.CharField(max_length=255)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
